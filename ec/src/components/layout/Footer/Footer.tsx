b50d1e333d11a16201736cfd39b669a1f11238b7
BRp99 <124196319+BRp99@users.noreply.github.com>
2023-12-14 23:48:07 +0000

    Add getCurrentYear() inside Footer to update dynamically the current year. Change description inside index.html

--- ec/src/components/layout/Footer/Footer.tsx
+++ ec/src/components/layout/Footer/Footer.tsx
@@ -1,5 +1,14 @@
 import { logoIcon } from "../../../icons/icons"
 import styles from "./Footer.module.css"
+
 export default function Footer() {
-  return <footer className={styles.container}>&copy; 2023 {logoIcon}</footer>
+  const getCurrentYear = () => {
+    return new Date().getFullYear()
+  }
+
+  return (
+    <footer className={styles.container}>
+      &copy; {getCurrentYear()} {logoIcon}
+    </footer>
+  )
 }
