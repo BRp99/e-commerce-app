639f1d60b02e28c713618b54b1a8012e08324b51
BRp99 <124196319+BRp99@users.noreply.github.com>
2024-01-13 15:47:05 +0000

    Add a new branch named e2e-testing + install cypress + do tests for CategoriesPage

--- ec/cypress.config.ts
+++ ec/cypress.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "cypress"
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   component: {
@@ -7,4 +7,10 @@ export default defineConfig({
       bundler: "webpack",
     },
   },
-})
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
